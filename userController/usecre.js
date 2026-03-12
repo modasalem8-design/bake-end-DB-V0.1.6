f4f234ae9538cb35213675e55aeaebea96ee0456
salem <salem.com>
2026-03-12 13:10:44 +0200

    update and send user for create account to DB

--- userController/usecre.js
+++ userController/usecre.js
@@ -1,28 +1,25 @@
 import db from "../sql&user/sql.js";
 import express from 'express'
-import router  from "../routs/user.js";
+import router from "../routs/user.js";
 import bcrypt from "bcrypt"
 const create = (express())
 create.use(express.json())
 create.post('/create', async (req, res) => {
     try {
         //  اتصال query
-        const user=(router)
         const { name, pass } = req.body;
+        const num = await db.query("SELECT * FROM `log` WHERE `name`=?", [name])
+        if (num.length > 0) {
+            res.status(301).json("the name is token for auther people")
+        }
         const query = ("INSERT INTO `log`( `name`, `pass`) VALUES ('?','?')")
-        
         const bcryptpass = bcrypt.hashSync(pass, 10);
-        const [result] = await db.query(name, bcryptpass)
+        const [result] = await db.query(query, [name, bcryptpass])
         res.status(202).json("connected in user")
         //  ايجاد مستخدم 
-        const fU =name.find((Date)=>name=Date.name)
-        if(fU){
-            res.status(503).json("dont use the name" )
-        }
-
-    } catch(error){
+    } catch (error) {
         console.error("err in user")
         res.status(501).json("err in user create account .js")
-     }
+    }
 })
 export default create;
